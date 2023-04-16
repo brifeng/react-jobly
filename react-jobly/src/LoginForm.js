125eee241a6e45dba0beb6d5e90dcd4db1f0783c
Brian <brian@email.com>
2023-04-16 11:46:42 -0400

    login and signup form

--- react-jobly/src/LoginForm.js
+++ react-jobly/src/LoginForm.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (username === 'myusername' && password === 'mypassword') {
+            setMessage('Login successful!');
+        } else {
+            setMessage('Invalid username or password');
+        }
+    };
+
     return (
-        <h1>LoginForm here</h1>
-    )
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username:
+                    <input type="text" value={username} onChange={handleUsernameChange} />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input type="password" value={password} onChange={handlePasswordChange} />
+                </label>
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+            <p>{message}</p>
+        </div>
+    );
 }
 
 export default LoginForm;
--- react-jobly/src/SignupForm.js
+++ react-jobly/src/SignupForm.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
 const SignupForm = () => {
+    const [name, setName] = useState('name');
+    const [email, setEmail] = useState('email@email.com');
+    const [password, setPassword] = useState('password');
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:3001/users', {
+                name,
+                email,
+                password,
+            });
+            if (response.status === 201) {
+                console.log('Registration successful!');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
-        <h1>SignupForm here</h1>
-    )
+        <form onSubmit={handleSubmit}>
+            <label>
+                Name:
+                <input type="text" value={name} onChange={handleNameChange} />
+            </label>
+            <br />
+            <label>
+                Email:
+                <input type="email" value={email} onChange={handleEmailChange} />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" value={password} onChange={handlePasswordChange} />
+            </label>
+            <br />
+            <input type="submit" value="Sign up" />
+        </form>
+    );
 }
 
 export default SignupForm;
